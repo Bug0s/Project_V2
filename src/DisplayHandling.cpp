344718d619a69fbe53dda0d833093863d464344a
Bug0s <bartos.bence@icloud.com>
2023-02-25 13:40:36 +0100

    Second commit

--- src/DisplayHandling.cpp
+++ src/DisplayHandling.cpp
@@ -24,7 +24,7 @@ namespace DisplayHandling {
         }
 
         void createHeadline() {
-            gfx->fillRect(0,0,480,20, RED);
+            gfx->fillRect(0,0,480,20, RED); //change
         }
     };
 }
