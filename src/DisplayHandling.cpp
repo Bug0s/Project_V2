846e3de87d82f7850d0ffe9333881c319192f378
bugos <bartosbence04@gmail.com>
2023-03-29 18:39:50 +0200

    mac startup

--- src/DisplayHandling.cpp
+++ src/DisplayHandling.cpp
@@ -31,8 +31,56 @@ namespace DisplayHandling {
 
         void createHeadline() {
             gfx->fillRect(0,0,480,20, RED);
-            jpegHandler.drawJpeg("/batteryCharging.jpg", 480-15, 0, gfx);
+            drawJpeg("/batteryCharging.jpg", 480-15, 0, gfx);
             
+        }
+
+        void downloadAndDisplayImage(char* link, int xpos, int ypos) {
+            Networking::Network().downloadImage(link);
+            this->drawJpeg("/image.jpg", xpos, ypos, this->gfx);
+        }
+
+
+        void drawJpeg(const char *filename, int xpos, int ypos, Arduino_GFX* tft)
+        {
+
+            Serial.println("===========================");
+            Serial.print("Drawing file: ");
+            Serial.println(filename);
+            Serial.println("===========================");
+
+            // Open the named file (the Jpeg decoder library will close it after rendering image)
+            fs::File jpegFile = SPIFFS.open(filename, "r"); // File handle reference for SPIFFS
+            //  File jpegFile = SD.open( filename, FILE_READ);  // or, file handle reference for SD library
+
+            // ESP32 always seems to return 1 for jpegFile so this null trap does not work
+            if (!jpegFile)
+            {
+                Serial.print("ERROR: File \"");
+                Serial.print(filename);
+                Serial.println("\" not found!");
+                return;
+            }
+
+            // Use one of the three following methods to initialise the decoder,
+            // the filename can be a String or character array type:
+
+            // boolean decoded = JpegDec.decodeFsFile(jpegFile); // Pass a SPIFFS file handle to the decoder,
+            // boolean decoded = JpegDec.decodeSdFile(jpegFile); // or pass the SD file handle to the decoder,
+            boolean decoded = JpegDec.decodeFsFile(filename); // or pass the filename (leading / distinguishes SPIFFS files)
+
+            if (decoded)
+            {
+                // print information about the image to the serial port
+                // jpegInfo();
+
+                // render the image onto the screen at given coordinates
+                JpegHandler::jpegRender(xpos, ypos, tft);
+            }
+            else
+            {
+                Serial.println("Jpeg file format not supported!");
+            }
         }
     };
 }
